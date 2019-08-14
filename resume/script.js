f5be7d8f2acfc483be6625042be2a13fdc904989
anglearzola <41270900+anglearzola@users.noreply.github.com>
2019-08-14 10:02:16 -0400

    Fixed html!

--- resume/script.js
+++ resume/script.js
@@ -1,31 +1,31 @@
 function chessButton() {
 	document.getElementById('yearsDone').innerHTML = "Years Done:" + " Freshman Year and Sophomore Year"
 	document.getElementById('description').innerHTML = "Description:" + " In chess club we played against each other. I was JV in chess"
-	document.getElementById('clubDiv').style.backgroundImage = "url('chess.jpeg')"
+	document.getElementById('clubDiv').style.backgroundImage = "url('/assets/chess.jpeg')"
 	document.getElementById('clubName').innerHTML = "Chess Club"
 }
 function computerButton() {
 	document.getElementById('yearsDone').innerHTML = "Years Done:" + " Freshman Year + Sophomore Year"
 	document.getElementById('description').innerHTML = "Description:" + " In computer club we learned a bit of Python and discussed things like AI and ate snacks"
-	document.getElementById('clubDiv').style.backgroundImage = "url('computer.jpg')"
+	document.getElementById('clubDiv').style.backgroundImage = "url(/assets/computer.jpg)"
 	document.getElementById('clubName').innerHTML = "Computer Club"
 }
 function dndButton() {
 	document.getElementById('yearsDone').innerHTML = "Years Done:" + " Freshman Year + Sophomore Year"
 	document.getElementById('description').innerHTML = "Description:" + " I was one of the people who started Dungeons and Dragons club by approaching teachers to host us. We played games of D&D and formed a D&D community at school by making people interested in it"
-	document.getElementById('clubDiv').style.backgroundImage = "url('dnd1.jpg')"
+	document.getElementById('clubDiv').style.backgroundImage = "url(/assets/dnd1.jpg)"
 	document.getElementById('clubName').innerHTML = "Dungeons and Dragons Club"
 }
 function helpingHandsButton() {
 	document.getElementById('yearsDone').innerHTML = "Years Done:" + " Freshman Year"
 	document.getElementById('description').innerHTML = "Description:" + " In helping hands club, we planned some school events and collected money at lunch for hurricane relief. I was able to help at Freshman orientation through this club"
-	document.getElementById('clubDiv').style.backgroundImage = "url('hh.jpg')"
+	document.getElementById('clubDiv').style.backgroundImage = "url(/assets/hh.jpg)"
 	document.getElementById('clubName').innerHTML = "Helping Hands Club"
 }
 function pingPongButton() {
 	document.getElementById('yearsDone').innerHTML = "Years Done:" + " Junior Year"
 	document.getElementById('description').innerHTML = "Description:" + " Two friends and I started this club in Junior year. It was already a club, but wasn't continued. We got the original teacher and restarted the club."
-	document.getElementById('clubDiv').style.backgroundImage = "url('pp.jpg')"
+	document.getElementById('clubDiv').style.backgroundImage = "url(/assets/pp.jpg)"
 	document.getElementById('clubName').innerHTML = "Ping Pong Club"
 }
 let currentJobIndex = 0
